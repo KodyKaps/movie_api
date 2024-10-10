9b4c2365d6ed1b2b04a30d10cff56af7e8d4363c
Kody Kapetanis <kodykapetanis@yahoo.com>
2024-10-10 07:04:54 -0400

    /
    /

--- movie_api/public/index.js
+++ movie_api/public/index.js
@@ -1,46 +1,27 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = 8080;
+
+const topMovies = [
+    { title: "The Shawshank Redemption", year: 1994 },
+    { title: "The Godfather", year: 1972 },
+    { title: "The Dark Knight", year: 2008 },
+    { title: "Pulp Fiction", year: 1994 },
+    { title: "Forrest Gump", year: 1994 },
+    { title: "Inception", year: 2010 },
+    { title: "Fight Club", year: 1999 },
+    { title: "The Matrix", year: 1999 },
+    { title: "Goodfellas", year: 1990 },
+    { title: "The Lord of the Rings: The Return of the King", year: 2003 }
+];
 
 
-function logRequest(requestUrl) {
-    const timestamp = new Date().toISOString(); 
-    const logEntry = `Time: ${timestamp} | URL: ${requestUrl}\n`;
-    
-
-
-    fs.appendFile('log.txt', logEntry, (err) => {
-        if (err) {
-            console.error('Error writing to log file', err);
-        }
-    });
-}
-
-
-const server = http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url, true);
-    const pathname = parsedUrl.pathname;
-
-    logRequest(request.url);
-
-    if (pathname.includes('documentation')) {
-        const filePath = path.join(__dirname, 'documentation.html');
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                response.writeHead(500, { 'Content-Type': 'text/plain' });
-                response.end('Error loading the documentation file');
-            } else {
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.end(data);
-            }
-        });
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/plain' });
-        response.end('Page Not Found');
-    }
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
-server.listen(8080, () => {
-    console.log('Server is running on port 8080');
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
